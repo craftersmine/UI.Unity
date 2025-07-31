d0c31419a850fad07bff4964499a03961014b81d
craftersmine <rodion.morozov.01@gmail.com>
2025-08-01 00:00:53 +0700

    Initial commit

---
+++ source/craftersmine.UI.Unity/craftersmine.Ui.Unity.DemoApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace craftersmine.UI.Unity.DemoApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ source/craftersmine.UI.Unity/craftersmine.Ui.Unity/ColorBlendExtension.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace craftersmine.UI.Unity
+{
+    internal class ColorBlendExtension : MarkupExtension
+    {
+        private readonly object? baseColor;
+        private readonly object? addedColor;
+        private readonly double factor;
+
+        public ColorBlendExtension(object? baseColor, object? addedColor, double factor = 0.5)
+        {
+            this.baseColor = baseColor;
+            this.addedColor = addedColor;
+            this.factor = factor;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var target = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
+            var color1 = Resolve(addedColor, target);
+            var color2 = Resolve(baseColor, target);
+
+            return UnityUiHelpers.Blend(color1, color2, factor);
+        }
+
+        private static Color Resolve(object value, IProvideValueTarget target)
+        {
+            if (value is Color c)
+                return c;
+            if (value is SolidColorBrush brush)
+                return brush.Color;
+            if (value is Expression expression)
+                return FindResource<Color>(ResolveResourceKey(expression), target);
+
+            return Colors.Transparent;
+        }
+
+        private static object ResolveResourceKey(object expression)
+        {
+            object? val = expression.GetType().GetProperty("ResourceKey")?.GetValue(expression);
+            if (val is null)
+                throw new ArgumentException(nameof(expression));
+
+            return val;
+        }
+
+        private static T? FindResource<T>(object key, IProvideValueTarget target)
+        {
+            Type? type;
+            T? value = default;
+            try
+            {
+                type = target.TargetObject.GetType();
+                if (target.TargetObject is FrameworkElement fe && fe.Parent != null)
+                    value = (T)fe.FindResource(key);
+                if (target.TargetObject is FrameworkContentElement fce && fce.Parent != null)
+                    value = (T)fce.FindResource(key);
+
+                value = (T)Application.Current.FindResource(key);
+            }
+            catch { }
+            return value;
+        }
+    }
+}

7b93852f38964b488c26ac52a61bb84668df1809
craftersmine <rodion.morozov.01@gmail.com>
2025-08-17 02:56:41 +0700

    Updated demo app

---
+++ source/craftersmine.UI.Unity/craftersmine.Ui.Unity.DemoApp/DebugItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.UI.Unity.DemoApp
+{
+    public class DebugItem
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}

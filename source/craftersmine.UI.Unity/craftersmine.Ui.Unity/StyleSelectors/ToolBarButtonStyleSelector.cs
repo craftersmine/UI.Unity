db879239a2eb6a6d7c4765d35c6d4c73dabfed8d
craftersmine <rodion.morozov.01@gmail.com>
2025-08-28 21:54:29 +0700

    Added StyleSelector for toolbar buttons

---
+++ source/craftersmine.UI.Unity/craftersmine.Ui.Unity/StyleSelectors/ToolBarButtonStyleSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace craftersmine.Ui.Unity.StyleSelectors
+{
+    internal class ToolBarButtonStyleSelector : StyleSelector
+    {
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            ItemsControl? toolbar = ItemsControl.ItemsControlFromItemContainer(container);
+            if (toolbar is null)
+                return base.SelectStyle(item, container);
+
+            if (toolbar.Items.Count < 2)
+                return base.SelectStyle(item, container);
+
+            int itemIndex = toolbar.Items.IndexOf(item);
+            Control itm = (Control)item;
+            Setter cornerRadius = (Setter)itm.Style.Setters.FirstOrDefault(s => ((Setter)s).Property == Border.CornerRadiusProperty);
+            switch (itemIndex)
+            {
+                case 0:
+                    return base.SelectStyle(item, container);
+                default:
+                    return base.SelectStyle(item, container);
+            }
+        }
+    }
+}

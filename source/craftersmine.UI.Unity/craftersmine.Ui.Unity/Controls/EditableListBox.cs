f58e377d7ebcf52711a44ca7fa24648edfb1510a
craftersmine <rodion.morozov.01@gmail.com>
2025-09-15 18:58:09 +0700

    Added Unity editable list box control stub

---
+++ source/craftersmine.UI.Unity/craftersmine.Ui.Unity/Controls/EditableListBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace craftersmine.Ui.Unity.Controls
+{
+    public class EditableListBox : ListBox
+    {
+        static EditableListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EditableListBox), new FrameworkPropertyMetadata(typeof(EditableListBox)));
+        }
+    }
+}

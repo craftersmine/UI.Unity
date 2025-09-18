30752a43302ed77a29f25875746a32a34a3e1951
craftersmine <rodion.morozov.01@gmail.com>
2025-09-18 21:34:12 +0700

    Added ColorPicker mockup template and backend class

---
+++ source/craftersmine.UI.Unity/craftersmine.Ui.Unity/Controls/ColorPicker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace craftersmine.Ui.Unity.Controls
+{
+    public class UnityColorPicker : Control
+    {
+        static UnityColorPicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(UnityColorPicker), new FrameworkPropertyMetadata(typeof(UnityColorPicker)));
+        }
+    }
+}
